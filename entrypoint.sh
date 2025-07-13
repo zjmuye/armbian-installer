30cd588bc04aa8f5be9d2bc0f604af3393f6e7da
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2025-07-13 20:25:44 +0800

    test 修复 buster 的源

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 set -e
 
 echo "🔧 修复 buster 的源..."
+
 cat > /etc/apt/sources.list <<EOF
 deb http://archive.debian.org/debian buster main contrib non-free
-deb http://security.debian.org/debian-security buster/updates main
+deb http://archive.debian.org/debian-security buster/updates main
 EOF
 
 echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/99no-check-valid-until
